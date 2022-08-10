67da08b46f7334fb81d3829cbb81a55477d9430b
Christopher Akinsanmi <christopher.akinsanmi@gmail.com>
2022-08-10 02:11:47 -0700

    printf

--- print_chars.c
+++ print_chars.c
@@ -9,10 +9,10 @@
  */
 int print_c(va_list c)
 {
-	char ch = (char)va_arg(c, int);
+char ch = (char)va_arg(c, int);
 
-	_putchar(ch);
-	return (1);
+_putchar(ch);
+return (1);
 }
 
 /**
@@ -23,16 +23,16 @@ int print_c(va_list c)
  */
 int print_s(va_list s)
 {
-	int count;
-	char *str = va_arg(s, char *);
+int count;
+char *str = va_arg(s, char *);
 
-	if (str == NULL)
-		str = "(null)";
-	for (count = 0; str[count]; count++)
-	{
-		_putchar(str[count]);
-	}
-	return (count);
+if (str == NULL)
+str = "(null)";
+for (count = 0; str[count]; count++)
+{
+_putchar(str[count]);
+}
+return (count);
 }
 
 /**
@@ -43,20 +43,20 @@ int print_s(va_list s)
  */
 static int hex_print(char c)
 {
-	int count;
-	char diff = 'A' - ':';
-	char d[2];
+int count;
+char diff = 'A' - ':';
+char d[2];
 
-	d[0] = c / 16;
-	d[1] = c % 16;
-	for (count = 0; count < 2; count++)
-	{
-		if (d[count] >= 10)
-			_putchar('0' + diff + d[count]);
-		else
-			_putchar('0' + d[count]);
-	}
-	return (count);
+d[0] = c / 16;
+d[1] = c % 16;
+for (count = 0; count < 2; count++)
+{
+if (d[count] >= 10)
+_putchar('0' + diff + d[count]);
+else
+_putchar('0' + d[count]);
+}
+return (count);
 }
 
 /**
@@ -67,28 +67,28 @@ static int hex_print(char c)
  */
 int print_S(va_list S)
 {
-	unsigned int i;
-	int count = 0;
-	char *str = va_arg(S, char *);
+unsigned int i;
+int count = 0;
+char *str = va_arg(S, char *);
 
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] < 32 || str[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			count += 2;
-			count += hex_print(str[i]);
-		}
-		else
-		{
-			_putchar(str[i]);
-			count++;
-		}
-	}
-	return (count);
+if (str == NULL)
+str = "(null)";
+for (i = 0; str[i]; i++)
+{
+if (str[i] < 32 || str[i] >= 127)
+{
+_putchar('\\');
+_putchar('x');
+count += 2;
+count += hex_print(str[i]);
+}
+else
+{
+_putchar(str[i]);
+count++;
+}
+}
+return (count);
 }
 
 /**
@@ -99,18 +99,18 @@ int print_S(va_list S)
  */
 int print_r(va_list r)
 {
-	char *str;
-	int i, count = 0;
+char *str;
+int i, count = 0;
 
-	str = va_arg(r, char *);
-	if (str == NULL)
-		str = ")llun(";
-	for (i = 0; str[i]; i++)
+str = va_arg(r, char *);
+if (str == NULL)
+str = ")llun(";
+for (i = 0; str[i]; i++)
 		;
-	for (i -= 1; i >= 0; i--)
+for (i -= 1; i >= 0; i--)
 	{
-		_putchar(str[i]);
-		count++;
-	}
-	return (count);
+_putchar(str[i]);
+count++;
+}
+return (count);
 }
